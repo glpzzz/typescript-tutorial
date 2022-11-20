1ca669bd31bd0deb709b2ea6c86f57c4de92fe06
Gabriel A. López López <glpz@daxslab.com>
2022-11-19 23:36:58 -0500

    End of lesson 06

--- sandbox.js
+++ sandbox.js
@@ -1,33 +1,15 @@
-// explicit types
-var character;
-var age;
-var isLoggedIn;
-// illegal. 5 is not string
-// character = 5;
-// legal. 'Mario' is a string
-character = 'Mario';
-// arrays. need to initialize empty to be able to use
-// push later
-var ninjas = [];
-console.log(ninjas);
-// illegal. 5 is not string
-// ninjas.push(5);
-// legal
-ninjas.push('Mario');
-console.log(ninjas);
-// union types. to instruct that a variable can be assigned values
-// several types.
+// removing the types restriction with any
+var age = 25;
+console.log(age);
+age = '1234';
+console.log(age);
+age = true;
+console.log(age);
+// just to use in rare cases.
+// using in arrays
 var mixed = [];
-mixed.push('Luigi');
-mixed.push(30);
-mixed.push(false);
-console.log(mixed);
-// in variables, () is not required
-var uid;
-// objects
-var ninjaOne;
-ninjaOne = { name: 'yoshi', age: 30 };
-// object with defined properties
-var ninjaTwo;
-// ilegal, skills wasn't defined as a property
-// ninjaTwo.skills = ['asdf'];
+mixed.push(1);
+mixed.push('1');
+mixed.push(true);
+// using in objects
+var ninja;
--- sandbox.ts
+++ sandbox.ts
@@ -1,42 +1,18 @@
-// explicit types
-let character: string;
-let age: number;
-let isLoggedIn: boolean;
+// removing the types restriction with any
+let age: any = 25;
+console.log(age);
+age = '1234';
+console.log(age);
+age = true;
+console.log(age);
 
-// illegal. 5 is not string
-// character = 5;
+// just to use in rare cases.
 
-// legal. 'Mario' is a string
-character = 'Mario';
+// using in arrays
+let mixed: any[] = [];
+mixed.push(1);
+mixed.push('1');
+mixed.push(true);
 
-// arrays. need to initialize empty to be able to use
-// push later
-let ninjas: string[] = [];
-console.log(ninjas);
-
-// illegal. 5 is not string
-// ninjas.push(5);
-
-// legal
-ninjas.push('Mario');
-console.log(ninjas);
-
-// union types. to instruct that a variable can be assigned values
-// several types.
-let mixed: (string | number | boolean)[] = [];
-mixed.push('Luigi');
-mixed.push(30);
-mixed.push(false);
-console.log(mixed);
-
-// in variables, () is not required
-let uid: string | number;
-
-// objects
-let ninjaOne: object;
-ninjaOne = {name: 'yoshi', age: 30};
-
-// object with defined properties
-let ninjaTwo: { name: string, age: number, belt: string };
-// illegal, skills wasn't defined as a property
-// ninjaTwo.skills = ['asdf'];
+// using in objects
+let ninja: { name: any, age: any, belt: any };
