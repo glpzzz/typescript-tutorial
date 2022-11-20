798bddbfb90d21a150e8ca7f7802223fec75e061
Gabriel A. López López <glpz@daxslab.com>
2022-11-20 00:09:57 -0500

    End of lesson 08

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,20 @@
 "use strict";
-console.log('Hello tsconfig');
+//typing for Functions
+let greet;
+greet = () => console.log('Hello, World!');
+// typing function parameters
+const add = (a, b) => console.log(a + b);
+add(5, 6);
+// optional parameter
+const func_with_optional = (p) => console.log(p);
+func_with_optional(); // p is undefined
+func_with_optional(10); // p is 10
+// parameter with default value
+const func_with_default_value = (p = 10) => console.log(p);
+func_with_default_value(); // p = 10 (default)
+func_with_default_value(20); // p = 20 (passed)
+// returning values typing (explicit)
+const minus = (a, b) => {
+    return a - b;
+};
+console.log(minus(10, 7));
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,23 @@
-console.log('Hello tsconfig');
+//typing for Functions
+let greet: Function;
+greet = () => console.log('Hello, World!');
+
+// typing function parameters
+const add = (a: number, b: number) => console.log(a + b);
+add(5, 6);
+
+// optional parameter
+const func_with_optional = (p?: number) => console.log(p);
+func_with_optional(); // p is undefined
+func_with_optional(10); // p is 10
+
+// parameter with default value
+const func_with_default_value = (p: number = 10) => console.log(p);
+func_with_default_value(); // p = 10 (default)
+func_with_default_value(20); // p = 20 (passed)
+
+// returning values typing (explicit)
+const minus = (a: number, b: number):number => {
+    return a - b;
+}
+console.log(minus(10,7));
